fc20c272ce3611d57767d636b74c588fe6efbe8d
Tyliszczak <kswiderski.de@gmail.com>
2026-08-08 17:18:52 +0200

    Odśwież cache dla nowej szaty graficznej

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
-const CACHE_NAME = "trasy-2.0-v3";
+const CACHE_NAME = "trasy-2.0-v4";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./app.js", "./routes.js", "./schedule.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))));
-self.addEventListener("message", (event) => {
-  if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
-});
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(networkFirst(event.request));
-});
+self.addEventListener("message", (event) => { if (event.data?.type === "SKIP_WAITING") self.skipWaiting(); });
+self.addEventListener("fetch", (event) => { if (event.request.method !== "GET") return; event.respondWith(networkFirst(event.request)); });
 
 async function networkFirst(request) {
   try {
