b9bf2255af161c52c591743ffc4166f05f40245c
Tyliszczak <kswiderski.de@gmail.com>
2026-08-10 19:56:26 +0200

    Odśwież cache lekkiej nawigacji

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='trasy-2.0-v18';
+const CACHE_NAME='trasy-2.0-v19';
 const APP_SHELL=['./','./index.html','./style.css','./app.js','./map-picker.js','./wake-style.js','./routes.js','./schedule.js','./manifest.json','./Tyliszczak.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL.map(u=>new Request(u,{cache:'reload'})))))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));await self.clients.claim()})()));
