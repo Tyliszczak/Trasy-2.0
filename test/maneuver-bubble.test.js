9e9ce6007a2062dbaae4d3c30a9509565acbcdd3
Tyliszczak <kswiderski.de@gmail.com>
2026-08-26 07:28:18 +0200

    Testuj stabilne zakotwiczenie dymka pod wskaźnikiem

--- test/maneuver-bubble.test.js
+++ test/maneuver-bubble.test.js
@@ -11,9 +11,19 @@ test('manewr i odległość są przenoszone z górnej belki do dymka pod wskaźn
   assert.match(bubble,/getElementById\('routeManeuver'\)/);
   assert.match(bubble,/getElementById\('routeManeuverDistance'\)/);
   assert.match(bubble,/bubble\.append\(maneuver,distance\)/);
-  assert.match(bubble,/anchor:'top'/);
-  assert.match(bubble,/offset:\[0,27\]/);
-  assert.match(bubble,/gps\.subscribe\(updatePosition/);
+  assert.match(bubble,/position:absolute/);
+  assert.match(bubble,/vehicle\.getBoundingClientRect\(\)/);
+  assert.match(bubble,/vehicleRect\.bottom-canvasRect\.top\+8/);
+  assert.doesNotMatch(bubble,/new maplibregl\.Marker/);
+  assert.doesNotMatch(bubble,/gps\.subscribe/);
+});
+
+test('dymek śledzi faktycznie narysowany wskaźnik i nie ma własnego markera GPS',()=>{
+  assert.match(bubble,/findVehicleElement/);
+  assert.match(bubble,/clip\.includes\('polygon'\)/);
+  assert.match(bubble,/el\.style\.width==='36px'/);
+  assert.match(bubble,/requestAnimationFrame\(positionBubble\)/);
+  assert.match(bubble,/if\(!attached\|\|!bubble\.isConnected\|\|panel\.hidden\)return/);
 });
 
 test('górna belka nie trzyma już marginesu po manewrze, a nazwa przystanku jest lekko mniejsza',()=>{
@@ -22,6 +32,6 @@ test('górna belka nie trzyma już marginesu po manewrze, a nazwa przystanku jes
 });
 
 test('dymek manewrów jest dostępny także z cache PWA',()=>{
-  assert.match(mapUi,/maneuver-bubble\.js\?v=1/);
+  assert.match(mapUi,/maneuver-bubble\.js\?v=2/);
   assert.match(sw,/\.\/maneuver-bubble\.js/);
 });
